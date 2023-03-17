c473debade078920787b5fc5f9ae78dbddbb82e5
Akalanka <akalankaperera128@gmail.com>
2023-03-17 13:12:42 +0530

    Removed success key from response

--- utils/handlers/response.js
+++ utils/handlers/response.js
@@ -1,6 +1,5 @@
 const responseHandler = ({ res, status = 200, message, data }) => {
-  const success = status >= 200 && status < 300
-  return res.status(status).json({ success, message, data })
+  return res.status(status).json({ message, data })
 }
 
 export default responseHandler
